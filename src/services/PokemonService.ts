9932a9f0f1900492186b7de35f11e25e718a681d
gmaggio <grld.mgio@yahoo.com>
2024-09-10 10:59:28 +0700

    [cognith-task] List Page Test: add axios mock

--- src/services/PokemonService.ts
+++ src/services/PokemonService.ts
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { PokemonModel, PokemonsResponseModel } from "./models";
 import { PokeApiPokemonMapper } from "./mappers/PokeApiPokemonMapper";
 import { PokemonData } from "../types";
@@ -8,19 +9,16 @@ class PokemonService {
   private static BASE_URL = 'https://pokeapi.co/api/v2';
 
   static async getPokemonsList(limit = 20, offset = 0): Promise<PokemonsResponseModel> {
-    const response = await fetch(
-      `${this.BASE_URL}/pokemon?limit=${limit}&offset=${offset}`,
+    const response = await axios.get<PokemonsResponseModel>(
+      `${this.BASE_URL}/pokemon`,
+      { params: { limit, offset } },
     );
-    const data = await response.json();
-
-    return data;
+    return response.data;
   }
 
   static async getPokemonDetails(url: string): Promise<PokemonData> {
-    const response = await fetch(url);
-    const data = await response.json();
-
-    return pokemonDetailsMapper.map(data);
+    const response = await axios.get<PokemonModel>(url);
+    return pokemonDetailsMapper.map(response.data);
   }
 };
 
