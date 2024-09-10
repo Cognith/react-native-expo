58e13c5190977bcab773503c7a3987e458b9d7a4
gmaggio <grld.mgio@yahoo.com>
2024-09-10 09:41:01 +0700

    [cognith-task] Details Page Test: add mock data

--- src/pages/PokemonDetails/__tests__/step-definitions/PokemonDetailsView.steps.tsx
+++ src/pages/PokemonDetails/__tests__/step-definitions/PokemonDetailsView.steps.tsx
@@ -1,13 +1,54 @@
 import { shallow, ShallowWrapper } from 'enzyme';
 import { defineFeature, loadFeature } from 'jest-cucumber';
 import PokemonDetailsView from '../../PokemonDetailsView';
+import { PokemonData } from '../../../../types';
+
+// Mocking PokemonData
+const mockPokemonData: PokemonData = {
+  id: '0001',
+  name: 'bulbasaur',
+  height: 7,
+  weight: 69,
+  image:
+    'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png',
+  stats: [
+    {
+      key: 'hp',
+      value: 45,
+    },
+    {
+      key: 'attack',
+      value: 49,
+    },
+    {
+      key: 'defense',
+      value: 49,
+    },
+    {
+      key: 'special-attack',
+      value: 65,
+    },
+    {
+      key: 'special-defense',
+      value: 65,
+    },
+    {
+      key: 'speed',
+      value: 45,
+    },
+  ],
+  abilities: ['overgrow', 'chlorophyll'],
+  types: ['grass', 'poison'],
+};
 
 const props = {
   navigation: {
     navigate: jest.fn(),
   } as any,
   route: {
-    params: {},
+    params: {
+      pokemon: mockPokemonData,
+    },
   } as any,
 };
 
